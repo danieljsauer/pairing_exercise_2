5582256c8a85565ac23f93719538d93ba1f9195d
Daniel Sauer <danieljsauer92@gmail.com>
2023-03-29 19:14:49 -0600

    Add method

--- friends.rb
+++ friends.rb
@@ -3,3 +3,7 @@ arrays = ["Wil", "Dylan", "Blake", "Davis", "Dan"]
 puts arrays[3]
 puts arrays[4]
 puts arrays[0]
+
+places_to_visit = ["Gainsville", "Fort Collins", "Grand Junction", "Seattle", "Maui"]
+
+puts places_to_visit.length 
